b23fa6a7075a83b8b17f6686b6a9b96f6801ae40
James Johnson <huminater@gmail.com>
2022-04-09 01:47:11 +0100

    Fix player 2 color networking

--- Assets/Scripts/PlayerVisualization.cs
+++ Assets/Scripts/PlayerVisualization.cs
@@ -6,15 +6,41 @@ using Unity.Netcode;
 public class PlayerVisualization : NetworkBehaviour
 {
 
-    [SerializeField] 
-    SpriteRenderer m_SpriteRenderer; 
+    [SerializeField]
+    SpriteRenderer m_SpriteRenderer;
+
+    NetworkVariable<Color> m_SpriteColor = new NetworkVariable<Color>(Color.red);
 
     void Start()
     {
         // Make player 2 blue
         if (!IsHost && IsOwner)
         {
-            m_SpriteRenderer.color = Color.blue;
+            SetPlayerTwoColorServerRpc();
         }
+
+        // Make sure the network variable is used for new clients joining
+        m_SpriteRenderer.color = m_SpriteColor.Value;
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        m_SpriteColor.OnValueChanged += OnColorChanged;
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        m_SpriteColor.OnValueChanged -= OnColorChanged;
+    }
+
+    public void OnColorChanged(Color previous, Color current)
+    {
+        m_SpriteRenderer.color = m_SpriteColor.Value;
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    public void SetPlayerTwoColorServerRpc()
+    {
+        m_SpriteColor.Value = Color.blue;
     }
 }
